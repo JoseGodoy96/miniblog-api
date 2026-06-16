bdd40da80b5e6d7934fd066f573256f3abed6ebf
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-16 20:59:39 +0100

    <feat>(UserService.java): Finalizacion de UserService CRUD API

--- miniblog/src/main/java/com/chema/db/miniblog/service/UserService.java
+++ miniblog/src/main/java/com/chema/db/miniblog/service/UserService.java
@@ -1,4 +1,49 @@
 package com.chema.db.miniblog.service;
 
+import com.chema.db.miniblog.model.User;
+import com.chema.db.miniblog.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User updatedUser) {
+
+        return userRepository.findById(id)
+                .map(user -> {
+
+                    user.setUsername(updatedUser.getUsername());
+                    user.setEmail(updatedUser.getEmail());
+
+                    return userRepository.save(user);
+                })
+                .orElseThrow(() ->
+                        new RuntimeException("User not found"));
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
