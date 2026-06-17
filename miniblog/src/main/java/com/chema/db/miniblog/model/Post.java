c346514838ecbdcdd6df0bb673469c71808871c6
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-17 22:48:49 +0100

    <feat>(Post.java): Creada la base de dato uno a muchos de post con @ManyToOne

--- miniblog/src/main/java/com/chema/db/miniblog/model/Post.java
+++ miniblog/src/main/java/com/chema/db/miniblog/model/Post.java
@@ -1,27 +1,29 @@
 package com.chema.db.miniblog.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "users")
+@Table(name = "posts")
 @Getter
 @Setter
-public class User {
+public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false, unique = true)
-    @NotBlank(message = "username is required")
-    private String username;
+    @NotBlank(message = "title is required")
+    private String title;
 
     @Column(nullable = false, unique = true)
-    @Email(message = "email must be valid")
-    @NotBlank(message = "email is required")
-    private String email;
+    @NotBlank(message = "content is required")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private User autor;
 }
