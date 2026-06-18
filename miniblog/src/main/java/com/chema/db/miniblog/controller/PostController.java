e83cb9bbcd9cfca16c97c43b4e34ea80631d2fb2
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-18 22:50:35 +0100

    <feat>(PostController.java): Final del postcontroller

--- miniblog/src/main/java/com/chema/db/miniblog/controller/PostController.java
+++ miniblog/src/main/java/com/chema/db/miniblog/controller/PostController.java
@@ -1,4 +1,32 @@
 package com.chema.db.miniblog.controller;
 
+import com.chema.db.miniblog.model.Post;
+import com.chema.db.miniblog.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/post")
 public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {this.postService = postService;}
+
+    @GetMapping
+    public List<Post> findAll() { return postService.getAllPost(); }
+
+    @GetMapping("/{id}")
+    public Post findOne(@PathVariable Long id) { return postService.getPostById(id); }
+
+    @PostMapping
+    public Post create(@RequestBody Post post) { return postService.createPost(post); }
+
+    @PutMapping("/{id}")
+    public Post update(@PathVariable Long id, @RequestBody Post post) { return postService.updatePost(id, post); }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {postService.deletePost(id);}
+
 }
