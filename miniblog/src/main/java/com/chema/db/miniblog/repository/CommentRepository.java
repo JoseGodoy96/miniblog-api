2611c27ea1ea13dc1dd13463b442e2df1a13a1e4
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-20 19:01:06 +0100

    <feat>(CommentRepository.java): Final de CommentRepository

---
+++ miniblog/src/main/java/com/chema/db/miniblog/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.chema.db.miniblog.repository;
+
+import com.chema.db.miniblog.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
