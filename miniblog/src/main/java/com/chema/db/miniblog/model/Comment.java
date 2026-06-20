e99220b149ecf04b2683cc616438e556432fd809
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-20 18:50:23 +0100

    <feat>(Comment.java): finalizacion del Comment.java me he dado cuenta de que autor esta en español pero no tengo gana de cambiarlo voy a mantener el error

--- miniblog/src/main/java/com/chema/db/miniblog/model/Comment.java
+++ miniblog/src/main/java/com/chema/db/miniblog/model/Comment.java
@@ -1,4 +1,29 @@
 package com.chema.db.miniblog.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotBlank(message = "content is required")
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private User autor;
 }
