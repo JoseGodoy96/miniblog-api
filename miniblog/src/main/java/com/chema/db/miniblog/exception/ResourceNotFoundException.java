6c2e280df600be9365fb27cfd23912f32f1271b5
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-21 23:10:25 +0100

    <feat>(ResourceNotFoundException.java, exception): Finalizacion de ResourceNotFoundException.java

---
+++ miniblog/src/main/java/com/chema/db/miniblog/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.chema.db.miniblog.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(resourceName + " not found with id: " + id);
+    }
+}
