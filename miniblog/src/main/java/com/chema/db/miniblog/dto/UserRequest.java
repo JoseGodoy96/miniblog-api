63ec1c12934791287da03da42c1212d8b08b7c9a
Jose Maria <josemariagodoy1996@gmail.com>
2026-06-23 23:10:45 +0100

    <feat>(UserReques.java, UserResponse.java): Final de los Request y response vamos a modificar ahora los Serive

--- miniblog/src/main/java/com/chema/db/miniblog/dto/UserRequest.java
+++ miniblog/src/main/java/com/chema/db/miniblog/dto/UserRequest.java
@@ -1,4 +1,18 @@
 package com.chema.db.miniblog.dto;
 
-public class UserRequest x{
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequest {
+
+    @NotBlank(message = "username is required")
+    private String username;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "email is required")
+    private String email;
 }
--- miniblog/src/main/java/com/chema/db/miniblog/dto/UserResponse.java
+++ miniblog/src/main/java/com/chema/db/miniblog/dto/UserResponse.java
@@ -1,4 +1,13 @@
 package com.chema.db.miniblog.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
 }
