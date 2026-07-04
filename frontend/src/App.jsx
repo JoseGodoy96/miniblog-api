10413d4bea9d7e63b31d1785ce30d6c671b738f9
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-04 12:24:57 +0100

    <feat>(App.jsx): Creado tambien el Get de posts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,20 @@ import { useState, useEffect } from 'react'
 
 function App() {
 	const [users, setUsers] = useState([])
+	const [posts, setPosts] = useState([])
 
 	useEffect(() => {
 		fetch('http://localhost:8080/api/user')
 		.then((response) => response.json())
 		.then((data) => setUsers(data))
 		.catch((error) => console.error('Error al cargar usuarios:', error))
+	}, [])
+
+	useEffect(() => {
+		fetch('http://localhost:8080/api/post')
+		.then((response) => response.json())
+		.then((data) => setPosts(data))
+		.catch((error) => console.error('Error al cargar los posts', error))
 	}, [])
 
 	return (
@@ -18,6 +26,14 @@ function App() {
 			{users.map((user) => (
 				<li key={user.id}>
 					{user.username} - {user.email}
+				</li>
+			))}
+		</ul>
+		<h2>Posts</h2>
+		<ul>
+			{posts.map((post) => (
+				<li key={post.id}>
+					{post.title} - {post.autorUsername}
 				</li>
 			))}
 		</ul>
