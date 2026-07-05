d0afd12f5e6b298ce530a01e078e1cbbeac1a13a
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-05 22:38:11 +0100

    <feat>(UserList.jsx): Final de UserList.jsx

---
+++ frontend/src/components/UserList.jsx
@@ -0,0 +1,13 @@
+function UserList({ users }) {
+	return (
+	<ul>
+		{users.map((user) => (
+        <li key={user.id}>
+			{user.username} - {user.email}
+        </li>
+		))}
+    </ul>
+	)
+}
+
+export default UserList
