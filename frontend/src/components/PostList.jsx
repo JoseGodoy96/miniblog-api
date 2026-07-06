248eb0b142c71a71c2d30883c47f9c5fdbf76b46
Jose Maria <josemariagodoy1996@gmail.com>
2026-07-06 22:36:05 +0100

    <feat>(PostList.jsx): Modificacion de PostList.jsx para incluir a borrarPost

--- frontend/src/components/PostList.jsx
+++ frontend/src/components/PostList.jsx
@@ -1,9 +1,10 @@
-function PostList({ posts }) {
+function PostList({ posts, borrarPosts }) {
 	return (
 	<ul>
 		{posts.map((post) => (
         <li key={post.id}>
 			{post.title} - {post.content} - {post.autorUsername}
+			<button onClick={() => borrarPosts(post.id)}>Eliminar</button>
         </li>
 		))}
     </ul>
